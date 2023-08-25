fe7e94fefd0916abc4920cc89ce002699b0ab0d2
Ron <81550623+thisellie@users.noreply.github.com>
2023-08-25 11:42:07 +0800

    Add addDom() function

--- src/to-do.js
+++ src/to-do.js
@@ -11,5 +11,43 @@ export class todo {
     tasks.push(new todo(title, desc, due, prio))
     console.log(tasks)
   }
+  static addDom() {
+    const list = document.querySelector('section')
+
+    const container = document.createElement('div')
+    container.classList.add('task')
+
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.id = `item-${tasks.length - 1}`
+    checkbox.classList.add('styled-checkbox')
+
+    const label = document.createElement('div')
+    label.classList.add('label')
+
+    const title = document.createElement('label')
+    title.htmlFor = `item-${tasks.length}`
+    title.classList.add('checkbox-label')
+    title.textContent = tasks[tasks.length - 1].title
+
+    const details = document.createElement('div')
+    details.classList.add('details')
+
+    const data = [
+      tasks[tasks.length - 1].desc,
+      tasks[tasks.length - 1].due,
+      tasks[tasks.length - 1].prio,
+    ]
+
+    data.forEach(property => {
+      const desc = document.createElement('p')
+      desc.textContent = property
+      details.append(desc)
+    })
+
+    label.append(title, details)
+    container.append(checkbox, label)
+    list.append(container)
+  }
 }
 
