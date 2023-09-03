5311a690429b5a3cc1eb6a3d18072bb39555c35f
Ron <81550623+thisellie@users.noreply.github.com>
2023-09-03 20:42:01 +0800

    Update functions

--- src/index.js
+++ src/index.js
@@ -2,53 +2,43 @@ import 'normalize.css'
 import './style.scss'
 import { todo } from './to-do'
 import { format, parseISO } from 'date-fns'
-import { loadTodo, selected } from './projects'
+import { loadTodo, selectTab, selected } from './projects'
 
 const addDialog = document.getElementById('add-dialog')
 const addButton = document.getElementById('add-button')
+const closeAdd = document.getElementById('add-close')
+const addForm = document.getElementById('add-form')
 
 addButton.addEventListener('click', () => addDialog.showModal())
 
-const closeAdd = document.getElementById('add-close')
-
-closeAdd.addEventListener('click', event => {
-  event.preventDefault()
+closeAdd.addEventListener('click', e => {
+  e.preventDefault()
   addDialog.close()
 })
 
-function formatDue() {
-  const date = document.getElementById('due').value
-  const parse = parseISO(date)
-  return format(parse, 'MMMM d')
-}
-
-export function formatPrio(priority, element) {
-  if (priority === 'Low') return element.classList.add('low')
-  if (priority === 'Medium') return element.classList.add('medium')
-  if (priority === 'High') return element.classList.add('high')
-}
-
-const addForm = document.getElementById('add-form')
-
-addForm.addEventListener('submit', event => {
-  event.preventDefault()
+addForm.addEventListener('submit', e => {
+  e.preventDefault()
 
   const title = document.getElementById('title').value
   const desc = document.getElementById('desc').value
-  const due = formatDue()
-  let priorityValue
-
-  for (const element of document.querySelectorAll('input[name="priority"]')) {
-    if (element.checked) {
-      priorityValue = element.value
-      break
-    }
-  }
+  const due = date(document.getElementById('due').value)
+  const priorityValue = priority()
 
   todo.addTask(title, desc, due, priorityValue)
   addForm.reset()
   addDialog.close()
 })
 
+export function date(value) {
+  if (!value) return (value = format(new Date(), 'MMMM d, yyyy'))
+  else return format(parseISO(value), 'MMMM d, yyyy')
+}
+
+export function priority() {
+  for (const element of document.querySelectorAll('input[name="priority"]'))
+    if (element.checked) return element.value
+}
+
 loadTodo(selected)
+selectTab(document.querySelectorAll('ul li')[0])
 
